15a02b51500225308fae34ffe8f2eda41fe74a56
Alexei Ledenev <alexei.led@gmail.com>
2025-07-01 08:39:41 +0300

    Update spotinfo to 2.0.1

--- Formula/spotinfo.rb
+++ Formula/spotinfo.rb
@@ -1,26 +1,26 @@
 class Spotinfo < Formula
   desc "CLI tool to explore AWS EC2 Spot instances with pricing and interruption analysis"
   homepage "https://github.com/alexei-led/spotinfo"
-  version "2.0.0"
+  version "2.0.1"
   license "Apache-2.0"
   
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/alexei-led/spotinfo/releases/download/#{version}/spotinfo_darwin_arm64"
-      sha256 "e45fbc830c71b8bfe4753694d618c3974600a2d3ee6446d2d120bdf3f9e8d234"
+      sha256 "b6a80fcf3cbac183f6a573a25da013d04fe2060b1478b50209858e3ee256990a"
     else
       url "https://github.com/alexei-led/spotinfo/releases/download/#{version}/spotinfo_darwin_amd64"
-      sha256 "6fcef4f7ebc0724f2a72824f7e5b386f2cd32aaaec402d1620ad41fa4aadd83d"
+      sha256 "a3f6aa758b0c922b34d6a7c12f5d7e382d8db3ec78ec4c1ee1d8680a8bbc4bd9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/alexei-led/spotinfo/releases/download/#{version}/spotinfo_linux_arm64"
-      sha256 "16e5534d11e8b718eee57b713b093a5539a6f7fae88f74a17ecaaaf4af161c28"
+      sha256 "20810ba3d95ac8bd851f0019c77f7860c21f58f298889578045a8adb65eb1bf5"
     else
       url "https://github.com/alexei-led/spotinfo/releases/download/#{version}/spotinfo_linux_amd64"
-      sha256 "292045af237466b0b501c1fe29a4339b8de3ba4a1a94a911379ccc46eeb35668"
+      sha256 "adc9cdd134936265976a161607f16457bf353fd46fa26a438520991f449b1019"
     end
   end
 
